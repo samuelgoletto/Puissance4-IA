c5ca8ee3aa3a716819aa20a6329231e7ffc84f8d
Samuel-Goletto <samuelgoletto@hotmail.fr>
2023-04-14 11:39:08 +0200

    finished main

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def main():
 
     while not is_game_over(board):
         # tour du joueur
-        column = player_choose_column(board)
-        play_in_column(board, column, 1)
+        player_column = player_choose_column(board)
+        play_in_column(board, player_column, 1)
         print_board(board)
         if is_winning_move(board, 1):
             print("Tu as gagné !")
@@ -16,7 +16,17 @@ def main():
             print("Match nul !")
             break
         # tour de l'IA
+        ai_column = ai_choose_column(board)
+        play_in_column(board, ai_column, 2)
+        print_board(board)
+        if is_winning_move(board, 2):
+            print("L'IA a gagné !")
+            break
+        if is_board_full(board):
+            print("Match nul !")
+            break
+    print("Fin de la partie !")
 
-
+        
 if __name__ == '__main__':
     main()
