1db21482bd912410c82661c68cd5af2f368f0498
Samuel-Goletto <samuelgoletto@hotmail.fr>
2023-04-14 11:05:33 +0200

    add end game checking methods

--- AI.py
+++ AI.py
@@ -41,3 +41,44 @@ def evaluate_pieces(pieces, player):
         return 2
     else:
         return 0
+
+def is_winning_move(board, player):
+    # Vérifier les alignements horizontaux
+    for row in range(6):
+        for col in range(4):
+            pieces = board[row][col:col+4]
+            if pieces.count(player) == 4:
+                return True
+    
+    # Vérifier les alignements verticaux
+    for row in range(3):
+        for col in range(7):
+            pieces = [board[row+i][col] for i in range(4)]
+            if pieces.count(player) == 4:
+                return True
+    
+    # Vérifier les alignements diagonaux (en haut à gauche vers le bas à droite)
+    for row in range(3):
+        for col in range(4):
+            pieces = [board[row+i][col+i] for i in range(4)]
+            if pieces.count(player) == 4:
+                return True
+    
+    # Vérifier les alignements diagonaux (en haut à droite vers le bas à gauche)
+    for row in range(3):
+        for col in range(3, 7):
+            pieces = [board[row+i][col-i] for i in range(4)]
+            if pieces.count(player) == 4:
+                return True
+    
+    return False
+
+def is_board_full(board):
+    for row in range(6):
+        for col in range(7):
+            if board[row][col] == 0:
+                return False
+    return True
+
+def is_game_over(board):
+    return is_winning_move(board, 1) or is_winning_move(board, 2) or is_board_full(board)
